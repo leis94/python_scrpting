1a7cdff742e5f6682b057c006b198516f8a7ea16
Cristian Daniel Silva Rueda <danielsilva@Cristians-MacBook-Pro.local>
2022-01-24 17:59:45 -0500

    Making func recursively

--- task1.py
+++ task1.py
@@ -1,15 +1,30 @@
 import os
+import re
 
-def read_files_in_path(path):
-    os.chdir(path)
 
-    filenames = os.listdir()
+def read_files_in_path(filenames):
+    if len(filenames) == 1:
+        filename = filenames[0]
+        try:
+            for i, line in enumerate(open(filename)):
+                for match in re.finditer(pattern, line):
+                #print('Found on line %s: %s' % (i+1, match.group()))
+                    print(f'File name: {filename}, Found on line {i+1}: {match.group()}')
+        except IsADirectoryError as e:
+            pass
 
-    return filenames
+    else:
+        mid = len(filenames) // 2
+        first_half = filenames[:mid]
+        second_half = filenames[mid:]
+
+        read_files_in_path(first_half)
+        read_files_in_path(second_half)
 
 
 input_path = r'/Users/danielsilva/Documents/Onboarding/Python Scripting'
-result = read_files_in_path(input_path)
-
-print(type(result))
-print(result)
+pattern = re.compile('[0-9]+')
+os.chdir(input_path)
+filenames = os.listdir()
+print(filenames)
+read_files_in_path(filenames)
