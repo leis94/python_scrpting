e20023b6b293e9741bb953bd2533b73493329eb5
Daniel Silva <bukaro.dany@gmail.com>
2022-01-25 09:32:01 -0500

    Add readme file, format document task1.py

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python Recursive Scripting
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+This is a task regarding python script for Python Scripting OnBoardning project in Moove-it to implement a recursive scripting.
+
+## Installation
+
+Dillinger requires [Python](https://www.python.org/) v3.7+ to run.
+
+Clone this repository in your local and run it.
+
+```sh
+cd "folder container script"
+python3 task1.py 'arg1' 'arg2'
+```
+arg1 = Is the Regular Expression whose will matches the content of the files provide in the path.
+arg2 = Is the path where will search into the content on the files the regex expresion.
+
+Note: Remember put the args inside '' or ""
+
+
+## License
+MIT
+**Free Software, Hell Yeah!**
--- task1.py
+++ task1.py
@@ -10,7 +10,6 @@ def read_files_in_path(filenames):
         try:
             for i, line in enumerate(open(filename)):
                 for match in re.finditer(pattern, line):
-                    #print('Found on line %s: %s' % (i+1, match.group()))
                     print(
                         f'File name: {filename}, Found on line {i+1}: {match.group()}')
         except IsADirectoryError as e:
@@ -26,8 +25,8 @@ def read_files_in_path(filenames):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="Path where looking into content files.")
 parser.add_argument("re", help="Regex Patter to match into content files.")
+parser.add_argument("path", help="Path where looking into content files.")
 args = parser.parse_args()
 pattern = re.compile(args.re)
 os.chdir(args.path)
